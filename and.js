15a923c3eb8ad69a5ee8c3f74c391a8cec347920
Shakil Ahmed <shakil200409@gmail.com>
2024-09-01 20:20:46 +0600

    tried and comparison

---
+++ and.js
@@ -0,0 +1,11 @@
+const age = 25;
+const height = 65;
+const salary = 26000;
+const isBcs = true;
+
+if(isBcs == true && height >= 62 && salary >= 25000){
+    console.log("Asho baba kobul");
+}
+else{
+    console.log("Bhag beta mokbul");
+}
